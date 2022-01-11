df8d6202e9939be3c16b8bd10a9fc7141b53e23f
Tatia Qajaia <tatiaqajaia0@gmail.com>
2022-01-12 01:35:40 +0400

    burger add

---
+++ script.js
@@ -0,0 +1,10 @@
+let navigation=document.getElementById('navigation')
+let burgerbutton=document.getElementById('burgerbar')
+
+burgerbutton.addEventListener('click',function(){
+    navigation.classList.toggle('active');
+})
+
+document.getElementById('send-btn').addEventListener('click',function(){
+    alert('Thank You!')
+})
